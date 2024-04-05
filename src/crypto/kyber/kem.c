3523435303012a68c51cf2680c403b385c117ba4
Graeme Connell <gram@signal.org>
2024-04-05 13:20:52 -0600

    Use noise_rand_bytes for Kyber.

--- src/crypto/kyber/kem.c
+++ src/crypto/kyber/kem.c
@@ -29,7 +29,7 @@ int crypto_kem_keypair(uint8_t *pk,
     sk[i+KYBER_INDCPA_SECRETKEYBYTES] = pk[i];
   hash_h(sk+KYBER_SECRETKEYBYTES-2*KYBER_SYMBYTES, pk, KYBER_PUBLICKEYBYTES);
   /* Value z for pseudo-random output on reject */
-  randombytes(sk+KYBER_SECRETKEYBYTES-KYBER_SYMBYTES, KYBER_SYMBYTES);
+  noise_rand_bytes(sk+KYBER_SECRETKEYBYTES-KYBER_SYMBYTES, KYBER_SYMBYTES);
   return 0;
 }
 
@@ -56,7 +56,7 @@ int crypto_kem_enc(uint8_t *ct,
   /* Will contain key, coins */
   uint8_t kr[2*KYBER_SYMBYTES];
 
-  randombytes(buf, KYBER_SYMBYTES);
+  noise_rand_bytes(buf, KYBER_SYMBYTES);
   /* Don't release system RNG output */
   hash_h(buf, buf, KYBER_SYMBYTES);
 
